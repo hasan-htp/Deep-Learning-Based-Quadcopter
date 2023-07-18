305a5e8b5e4c5c9543dc81e1b7ecec2edd260323
hasan hatipoglu <37053503+hasan-htp@users.noreply.github.com>
2023-07-18 10:11:20 +0200

    Update ESC_MOTOR_TEST.ino

--- Flight control/ESC_MOTOR_TEST.ino
+++ Flight control/ESC_MOTOR_TEST.ino
@@ -1,29 +1,11 @@
-/**
- * Usage, according to documentation(https://www.firediy.fr/files/drone/HW-01-V4.pdf) : 
- *     1. Plug your Arduino to your computer with USB cable, open terminal, then type 1 to send max throttle to every ESC to enter programming mode
- *     2. Power up your ESCs. You must hear "beep1 beep2 beep3" tones meaning the power supply is OK
- *     3. After 2sec, "beep beep" tone emits, meaning the throttle highest point has been correctly confirmed
- *     4. Type 0 to send min throttle
- *     5. Several "beep" tones emits, which means the quantity of the lithium battery cells (3 beeps for a 3 cells LiPo)
- *     6. A long beep tone emits meaning the throttle lowest point has been correctly confirmed
- *     7. Type 2 to launch test function. This will send min to max throttle to ESCs to test them
- *
- * @author lobodol <grobodol@gmail.com>
- */
-// ---------------------------------------------------------------------------
 #include <Servo.h>
-// ---------------------------------------------------------------------------
 // Customize here pulse lengths as needed
 #define MIN_PULSE_LENGTH 1000 // Minimum pulse length in µs
 #define MAX_PULSE_LENGTH 2000 // Maximum pulse length in µs
 // ---------------------------------------------------------------------------
 Servo motA, motB, motC, motD;
 char data;
-// ---------------------------------------------------------------------------
 
-/**
- * Initialisation routine
- */
 void setup() {
     Serial.begin(9600);
     
@@ -35,9 +17,6 @@ void setup() {
     displayInstructions();
 }
 
-/**
- * Main function
- */
 void loop() {
     if (Serial.available()) {
         data = Serial.read();
@@ -74,9 +53,6 @@ void loop() {
 
 }
 
-/**
- * Test function: send min throttle to max throttle to each ESC.
- */
 void test()
 {
     for (int i = MIN_PULSE_LENGTH; i <= MAX_PULSE_LENGTH; i += 5) {
@@ -98,9 +74,6 @@ void test()
     motD.writeMicroseconds(MIN_PULSE_LENGTH);
 }
 
-/**
- * Displays instructions to user
- */
 void displayInstructions()
 {  
     Serial.println("READY - PLEASE SEND INSTRUCTIONS AS FOLLOWING :");
